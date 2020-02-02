fa5015eca4c0455a6a33960780a1f1980009a9cb
gbeznicki <g.beznicki@gmail.com>
2020-02-02 18:12:00 +0100

    added test project

---
+++ SportStore.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SportStore.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
