028fc164540f90d6aeb19c61326800baa761e9ab
gbeznicki <g.beznicki@gmail.com>
2020-02-03 21:21:49 +0100

    added pagination

--- SportStore/Controllers/ProductController.cs
+++ SportStore/Controllers/ProductController.cs
@@ -10,11 +10,16 @@ namespace SportStore.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
         public ProductController(IProductRepository repository)
         {
             this.repository = repository;
         }
 
-        public ViewResult List() => View(repository.Products);
+        public ViewResult List(int productPage = 1) => 
+            View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((PageSize - 1) * productPage)
+                .Take(PageSize));
     }
 }
