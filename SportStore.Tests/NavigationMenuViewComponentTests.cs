821d418872a5dd6bcad1ae530e8840133c3179c6
Gustaw Beźnicki <g.beznicki@gmail.com>
2020-02-05 16:25:12 +0100

    added category menu view component and tests to it

---
+++ SportStore.Tests/NavigationMenuViewComponentTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportStore.Components;
+using SportStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SportStore.Tests
+{
+    [TestClass]
+    public class NavigationMenuViewComponentTests
+    {
+        [TestMethod]
+        public void CanSelectCategories()
+        {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product {ProductID = 2, Name = "P2", Category = "Apples"},
+                new Product {ProductID = 3, Name = "P3", Category = "Plums"},
+                new Product {ProductID = 4, Name = "P4", Category = "Oranges"},
+            }).AsQueryable<Product>());
+
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
+
+            //Act
+            string[] results = ((IEnumerable<string>)(target.Invoke() as ViewViewComponentResult).ViewData.Model).ToArray();
+
+            // Assert
+            Assert.IsTrue(Enumerable.SequenceEqual(new string[] { "Apples",
+                "Oranges", "Plums" }, results));
+        }
+    }
+}
---
+++ SportStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IProductRepository productRepository;
+
+        public NavigationMenuViewComponent(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(productRepository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
