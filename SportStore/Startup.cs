a7e3d3d3fd676c41bcc135aeede55d4460bad9b8
Gustaw Beźnicki <g.beznicki@gmail.com>
2020-02-06 15:55:54 +0100

    added a cart

--- SportStore/Startup.cs
+++ SportStore/Startup.cs
@@ -26,6 +26,8 @@ namespace SportStore
                 Configuration["Data:SportStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +36,7 @@ namespace SportStore
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
